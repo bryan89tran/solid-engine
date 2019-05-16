77bb5dce14ef1968a795218660f02937c316c528
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-05-15 21:55:06 -0700

    Add Special tags to admin

---
+++ GraniteHouse/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin
+{
+    [Area("Admin")]
+    public class SpecialTagsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SpecialTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+
+            var allItems = _db.SpecialTags.ToList();
+            return View(allItems);
+        }
+
+        
+    }
+}
