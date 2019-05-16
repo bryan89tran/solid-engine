df1e3f0d0415718fe075a2188e723c3b8e28b67a
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-05-15 21:32:40 -0700

    Add create, edit, and delete to controller

--- GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -43,5 +43,84 @@ namespace GraniteHouse.Areas.Admin.Controllers
 
             return View(productTypes);
         }
+
+        //Get Edit action method
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if(productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Post Create action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductTypes productTypes)
+        {
+            if(id != productTypes.Id)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+
+        //Get Details action method
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Get Delete action method
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if(productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteStuff(int id)
+        {
+            var productType = await _db.ProductTypes.FindAsync(id);
+            _db.ProductTypes.Remove(productType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
